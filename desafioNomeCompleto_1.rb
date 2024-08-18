b54a3a55dc40f7e1e6ba13a163e1a213e2b6a9f8
Jhonatan <jhonatanat@gmail.com>
2024-08-18 15:10:49 -0300

    Atualização e execução do Desafio

---
+++ desafioNomeCompleto_1.rb
@@ -0,0 +1,14 @@
+#Criar um programa na linguagem Ruby que dê a saída do nome complete do usuário e a sua idade
+
+print "Informe seu Nome por favor: ";
+name = gets.chomp;
+
+print "Agora informe seu Sobrenome completo: ";
+lastName = gets.chomp; 
+
+print "Informe sua Idades: ";
+age = gets.chomp;
+
+puts "Olá #{name} #{lastName}! Seja bem vindo! Sua idade é #{age}."
+
+
